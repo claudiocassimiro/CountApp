058021b43d3813dc671fb6bc9ecf5296f60be44a
Joao <contatojoem@gmail.com>
2022-06-17 14:49:16 -0300

    Adicionado a opção +2 e -2

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const number = document.getElementById('number');
 const buttonIncrement = document.getElementById('buttonIncrement');
 const buttonZero = document.getElementById('buttonZero');
 const buttonDecrement = document.getElementById('buttonDecrement');
+const buttonDecrementPlus = document.getElementById('buttonDecrementPlus');
+const buttonIncrementPlus = document.getElementById('buttonIncrementPlus');
 
 const incrementNumber = () => {
   const newNumber = Number(number.innerText) + 1;
+  number.innerText = newNumber;
+};
+
+const incrementNumberPlus = () => {
+  const newNumber = Number(number.innerText) + 2;
   number.innerText = newNumber;
 };
 
@@ -19,8 +26,22 @@ const decrementNumber = () => {
   return (number.innerText = newNumber);
 };
 
+const decrementNumberPlus = () => {
+  const newNumber = Number(number.innerText) - 2;
+
+  if (newNumber <= 1) {
+    return (number.innerText = 0);
+  }
+
+  return (number.innerText = newNumber);
+
+}
 buttonIncrement.addEventListener('click', incrementNumber);
 
 buttonZero.addEventListener('click', () => (number.innerText = 0));
 
 buttonDecrement.addEventListener('click', decrementNumber);
+
+buttonIncrementPlus.addEventListener('click', incrementNumberPlus);
+
+buttonDecrementPlus.addEventListener('click', decrementNumberPlus)
